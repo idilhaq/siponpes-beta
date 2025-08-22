03581d9c1d5f441b9b70bbeec47581cc6ef49738
idilhaq <idil.amir@gotocompany.com>
2025-08-22 23:50:26 +0700

    sample

--- Modules/Psb/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Psb\Http\Controllers\PsbController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('psbs', PsbController::class)->names('psb');
-});
